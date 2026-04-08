35dff641fde83fae164a42a4941264e476dc8ebc
Tfronta <tfronta@gmail.com>
2026-04-08 14:30:34 -0300

    Fix: GenerateToolConfig.sh now writes directly to config/UserToolsConfig.txt
    Previously the script wrote to ./UserToolsConfig.txt in the current
    directory, requiring a manual mv step that was error-prone and
    inconsistent. Now writes directly to config/UserToolsConfig.txt.

--- setup/GenerateToolConfig.sh
+++ setup/GenerateToolConfig.sh
@@ -4,17 +4,17 @@
 #echo -e "This will create a ToolConfig file to run STRspy..."
 
 SAMTOOLS=$(which samtools)
-echo -e "SAMTOOLS="$SAMTOOLS > UserToolsConfig.txt
+echo -e "SAMTOOLS="$SAMTOOLS > config/UserToolsConfig.txt
 BEDTOOLS=$(which bedtools)
-echo -e "BEDTOOLS="$BEDTOOLS >> UserToolsConfig.txt
+echo -e "BEDTOOLS="$BEDTOOLS >> config/UserToolsConfig.txt
 XATLAS=$(which xatlas)
-echo -e "XATLAS="$XATLAS >> UserToolsConfig.txt
+echo -e "XATLAS="$XATLAS >> config/UserToolsConfig.txt
 PARALLEL=$(which parallel)
-echo -e "PARALLEL="$PARALLEL >> UserToolsConfig.txt
+echo -e "PARALLEL="$PARALLEL >> config/UserToolsConfig.txt
 MINIMAP=$(which minimap2)
-echo -e "MINIMAP="$MINIMAP >> UserToolsConfig.txt
+echo -e "MINIMAP="$MINIMAP >> config/UserToolsConfig.txt
 SEQKIT=$(which seqkit)
-echo -e "SEQKIT="$SEQKIT >> UserToolsConfig.txt
+echo -e "SEQKIT="$SEQKIT >> config/UserToolsConfig.txt
 
 echo -e "ToolConfig file is generated."
 
